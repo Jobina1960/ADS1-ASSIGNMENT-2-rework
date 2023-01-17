328d69df66ec8928ff2cbbaa47a839c2e541da18
jobina <jp22aay@herts.ac.uk>
2023-01-17 12:58:00 +0000

    Reworking assignment, started the code

---
+++ jobina_rework.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 12:46:53 2023
+
+@author: jobina
+"""
+
+"""
+    Reworking ADS1 Assignment 2 by taking old code as reference and making necessary changes 
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
