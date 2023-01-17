3e350853d894e7936743de99a57c465da0420143
jobina <jp22aay@herts.ac.uk>
2023-01-17 16:34:01 +0000

    increased number of years used in plotting, added more comments, made slight changes

--- jobina_rework.py
+++ jobina_rework.py
@@ -1,8 +1,8 @@
-
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
+#defining file read function
 def readfile(file_name):
     """
     This function reads an excel file and transpose it.
@@ -13,12 +13,11 @@ def readfile(file_name):
     Dataframes(original and transpose)
     """
     d = pd.read_excel(file_name)
-    d = d.drop(['Series Name', 'Series Code',
-                     'Country Code'], axis=1).dropna()
+    d = d.drop(['Series Name', 'Series Code','Country Code'], axis=1).dropna()
     d_trans = d.transpose()
     return d, d_trans
 
-
+#defining bar graph function
 def bar_graph(d_name, title, fig_name, y_label):
     """
     This function plots a bargrapgh.
@@ -26,26 +25,27 @@ def bar_graph(d_name, title, fig_name, y_label):
     """
     plt.figure(figsize=(17, 18))
     yrs = ["1990 [YR1990]",
-             "2013 [YR2013]", "2014 [YR2014]", "2015 [YR2015]"]
+             "1994 [YR1994]", "1998 [YR1998]", "2002 [YR2002]","2006 [YR2006]","2010 [YR2010]","2014 [YR2014]", "2018 [YR2018]"]
     d_name.plot(x="Country Name", y=yrs, kind='bar')
     plt.title(title, fontsize=12)
     plt.xlabel('Countries', fontsize=10)
     plt.xticks(fontsize=10, rotation=90)
     plt.ylabel(y_label, fontsize=10)
     plt.yticks(fontsize=10)
+    plt.xticks(rotation=45)
     plt.legend(frameon=False, fontsize=10)
     #plt.savefig(fig_name, bbox_inches="tight", dpi=200)
     plt.show()
 
-
+#defining line graph function
 def line_graph(d_name, title, fig_name, y_label):
     """
     This function plots a linegrapgh.
     Parameters: d_name, title, fig_name, y_label
     """
     plt.figure(figsize=(17, 18))
-    yrs = ["2012 [YR2012]", "2013 [YR2013]", "2014 [YR2014]",
-             "2015 [YR2015]"]
+    yrs = ["1990 [YR1990]",
+             "1994 [YR1994]", "1998 [YR1998]", "2002 [YR2002]","2006 [YR2006]","2010 [YR2010]","2014 [YR2014]", "2018 [YR2018]"]
     d_name.plot(x="Country Name", y=yrs, kind='line')
     plt.title(title, fontsize=12)
     plt.xlabel('Countries', fontsize=10)
@@ -53,10 +53,13 @@ def line_graph(d_name, title, fig_name, y_label):
                d_name["Country Name"], rotation=90)
     plt.ylabel(y_label, fontsize=10)
     plt.yticks(fontsize=10)
+    plt.xticks(rotation=45)
     plt.legend(frameon=False, fontsize=10)
     #plt.savefig(fig_name, bbox_inches="tight", dpi=200)
     plt.show()
 
+
+    
 
 # Reading the excel file 
 population, population_trans = readfile(
@@ -79,5 +82,4 @@ bar_graph(population, "Total population",
          "population Bargraph.png","world")
 bar_graph(emission, "CO2 emission",
          "CO2 emission Bargraph.png","kt")
-
 
